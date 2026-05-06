352589078071d0665d98c4cd05e851b0c50addc1
Heahaidu <heahaidu20@gmail.com>
2026-05-06 23:12:43 +0700

    refactor

--- src/main/java/me/heahaidu/springboothealthbootstrap/SpingbootHealthBootstrapApplication.java
+++ src/main/java/me/heahaidu/springboothealthbootstrap/SpingbootHealthBootstrapApplication.java
@@ -1,13 +1,13 @@
-package me.heahaidu.spingboothealthbootstrap;
+package me.heahaidu.springboothealthbootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpingbootHealthBootstrapApplication {
+public class SpringbootHealthBootstrapApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpingbootHealthBootstrapApplication.class, args);
+        SpringApplication.run(SpringbootHealthBootstrapApplication.class, args);
     }
 
 }
